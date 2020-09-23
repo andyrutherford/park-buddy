495248fec663a24f431ffdc74dc826a1d2f532fb
Andy <andrew.rutherford.6@gmail.com>
2020-09-23 11:39:23 +0200

    init express server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hey there partner');
+});
+
+app.listen(PORT, console.log(`Server running on port ${PORT}.`));
