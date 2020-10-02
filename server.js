ebe8966a291f8591b39d4e24417c7ed193f3e669
Andy <andrew.rutherford.6@gmail.com>
2020-10-02 12:43:16 +0200

    .

--- server.js
+++ server.js
@@ -27,6 +27,15 @@ app.use(
 // parse cookies
 app.use(cookieParser());
 
+app.use(
+  session({
+    secret: 'keyboard cat',
+    cookie: { httpOnly: false },
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
 // CORS
 app.use(
   cors({
@@ -42,6 +51,21 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept'
+  );
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+});
 
 // Passport middleware
 
